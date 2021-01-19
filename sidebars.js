4dbe59238621e1973eda28166a9cfb0de9414a4f
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2021-01-19 23:53:55 +0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,6 @@
 module.exports = {
-  docs: [
-    {
-      type: 'category',
-      label: 'About Flaw Tech',
-      items: ['doc1'],
-    },
-    {
-      type: 'category',
-      label: 'Legal',
-      items: ['doc2', 'doc3'],
-    },
-  ],
+  someSidebar: {
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
 };
