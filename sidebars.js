1184bdea1856f754a2bf3e2644d6bc37f8a87412
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2021-01-20 00:18:55 +0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    'About Flaw Tech': ['doc-about-flaw-tech-faq'],
+    'About Flaw Tech': ['about-flaw-tech-faq'],
     'Legal': ['doc2', 'doc3', 'mdx'],
   },
 };
