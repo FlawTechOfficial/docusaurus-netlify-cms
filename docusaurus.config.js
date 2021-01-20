64fca9a2880dff2a5164ef51c25233534a597d6d
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2021-01-20 17:34:00 +0500

    updated configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ module.exports = {
           items: [
             {
               label: 'Contact Us',
-              to: 'docs/about-flaw-tech-contact',
+              to: 'support',
             },
           ],
         },
--- support-sidebar.js
+++ support-sidebar.js
@@ -1,5 +1,5 @@
 module.exports = {
-  supportSidebar: [
+  support: [
     'support-forum',
     'support-email',
   ],
