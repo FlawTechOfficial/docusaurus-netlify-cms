3aa4ae29c508cb18fd41b03f33c8a4a2488511b5
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2021-01-21 00:23:26 +0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   secondSidebar: {
     'Introduction': ['welcome'],
-    'Flaw Tech': ['flaw-tech-who-are-we', 'flaw-tech-code-of-conduct'],
+    'Company': ['flaw-tech-who-are-we', 'flaw-tech-code-of-conduct'],
     'Legal': ['legal-privacy-policy', 'legal-terms-of-use', 'legal-cookies-policy', 'legal-trademarks'],
   },
 };
